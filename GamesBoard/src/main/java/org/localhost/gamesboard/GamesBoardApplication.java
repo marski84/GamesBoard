79100d77d661223410e5c7123acce033ac20e494
marski84 <marski705@gmail.com>
2024-08-10 13:24:07 +0200

    init

---
+++ GamesBoard/src/main/java/org/localhost/gamesboard/GamesBoardApplication.java
@@ -0,0 +1,13 @@
+package org.localhost.gamesboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamesBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamesBoardApplication.class, args);
+	}
+
+}
---
+++ GamesBoard/src/test/java/org/localhost/gamesboard/GamesBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package org.localhost.gamesboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamesBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
