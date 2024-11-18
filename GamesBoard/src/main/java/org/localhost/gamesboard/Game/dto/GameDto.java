70f84ce5e6deec99214c5c214120672d5c8987ed
marski84 <marski705@gmail.com>
2024-11-18 11:15:20 +0100

    refactored dto

--- GamesBoard/src/main/java/org/localhost/gamesboard/Game/dto/GameDto.java
+++ GamesBoard/src/main/java/org/localhost/gamesboard/Game/dto/GameDto.java
@@ -6,16 +6,32 @@ import org.localhost.gamesboard.Game.model.Game;
 import org.localhost.gamesboard.GameManager.model.PlayerScore;
 import org.localhost.gamesboard.Player.model.Player;
 
-import java.time.Instant;
+import java.time.ZonedDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 @Getter
 @Builder
 public class GameDto {
     private String gameName;
-    private Instant creationDate;
-    private Instant startDate;
-    private Instant finishDate;
+    private ZonedDateTime creationDate;
+    private ZonedDateTime startDate;
+    private ZonedDateTime finishDate;
     private List<PlayerScore> playerScores;
     private List<Player> players;
+    //from...
+    //with...
+
+    public static GameDto fromGame(Game game) {
+        return GameDto.builder()
+                .gameName(game.getGameName())
+                .creationDate(game.getCreatedAt())
+                .startDate(game.getGameStartDate())
+                .finishDate(game.getGameFinishDate())
+                .playerScores(new ArrayList<>(game.getGameScore()))
+                .players(new ArrayList<>(game.getPlayers()))
+                .build();
+    }
+
+
 }
